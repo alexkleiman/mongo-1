01ca7b51af2015aca869c6a096185e5333694f6b
Antonio Juliano <ajuliano@princeton.edu>
2014-07-11 13:43:01 -0400

    works except startup

--- src/mongo/db/storage/twitter/twitter_cud.cpp
+++ src/mongo/db/storage/twitter/twitter_cud.cpp
@@ -31,7 +31,7 @@
 #include "mongo/db/storage/twitter/twitter_cud.h"
 
 namespace mongo {
-	TwitterCUD::TwitterCUD(string username, string password) {
+	TwitterCUD::TwitterCUD(string username, string password) {/*
 		boost::mutex::scoped_lock lk( _curlLock );
 		t = twitCurl();
 		t.setTwitterUsername(username);
@@ -44,7 +44,8 @@ namespace mongo {
 		t.getOAuth().setConsumerKey( std::string( "t6PEWWSkw1KAvcyEKiAR4Apv9" ) );
     	t.getOAuth().setConsumerSecret( std::string( "ETmHXdWCo3BZRfuAcypznSywmYnFa2SHidJLYclJt4D2623XK6" ) );
 
-		/* Step 1: Check if we alredy have OAuth access token from a previous run */
+		
+
 	    std::string myOAuthAccessTokenKey("");
 	    std::string myOAuthAccessTokenSecret("");
 	    std::ifstream oAuthTokenKeyIn;
@@ -66,7 +67,7 @@ namespace mongo {
 
 	    if( myOAuthAccessTokenKey.size() && myOAuthAccessTokenSecret.size() )
 	    {
-	        /* If we already have these keys, then no need to go through auth again */
+	        
 	        printf( "\nUsing:\nKey: %s\nSecret: %s\n\n", myOAuthAccessTokenKey.c_str(), myOAuthAccessTokenSecret.c_str() );
 
 	        t.getOAuth().setOAuthTokenKey( myOAuthAccessTokenKey );
@@ -74,18 +75,18 @@ namespace mongo {
 	    }
 	    else
 	    {
-	        /* Step 2: Get request token key and secret */
+	        
 	        std::string authUrl;
 	        t.oAuthRequestToken( authUrl );
 
-	        /* Step 3: Get PIN  */
+	       
 	        memset( tmpBuf, 0, 1024 );
 	        printf( "\nDo you want to visit twitter.com for PIN (0 for no; 1 for yes): " );
 	        gets( tmpBuf );
 	        tmpStr = tmpBuf;
 	        if( std::string::npos != tmpStr.find( "1" ) )
 	        {
-	            /* Ask user to visit twitter.com auth page and get PIN */
+	            
 	            memset( tmpBuf, 0, 1024 );
 	            printf( "\nPlease visit this link in web browser and authorize this application:\n%s", authUrl.c_str() );
 	            printf( "\nEnter the PIN provided by twitter: " );
@@ -95,18 +96,18 @@ namespace mongo {
 	        }
 	        else
 	        {
-	            /* Else, pass auth url to twitCurl and get it via twitCurl PIN handling */
+	            
 	            t.oAuthHandlePIN( authUrl );
 	        }
 
-	        /* Step 4: Exchange request token with access token */
+	       
 	        t.oAuthAccessToken();
 
-	        /* Step 5: Now, save this access token key and secret for future use without PIN */
+	 
 	        t.getOAuth().getOAuthTokenKey( myOAuthAccessTokenKey );
 	        t.getOAuth().getOAuthTokenSecret( myOAuthAccessTokenSecret );
 
-	        /* Step 6: Save these keys in a file or wherever */
+	    
 	        std::ofstream oAuthTokenKeyOut;
 	        std::ofstream oAuthTokenSecretOut;
 
@@ -122,9 +123,7 @@ namespace mongo {
 	        oAuthTokenKeyOut.close();
 	        oAuthTokenSecretOut.close();
 	    }
-	    /* OAuth flow ends */
-
-	    /* Account credentials verification */
+	    
 	    bool b = false;
 	    try {
 	    	b = t.accountVerifyCredGet();
@@ -145,25 +144,21 @@ namespace mongo {
 	        t.getLastCurlError( replyMsg );
 	        printf( "\ntwitterClient:: twitCurl::accountVerifyCredGet error:\n%s\n", replyMsg.c_str() );
 	    }
-
+	*/
+	    
 	}
 
     bool TwitterCUD::insert(BSONObj obj, DiskLoc loc, string ns) {
-    	boost::mutex::scoped_lock lk( _curlLock );
+    	
         string toTweet = _toTweetString(obj, loc, ns);
+        if (toTweet.find("local.startup") != std::string::npos)
+        	return true;
 
-        bool ok = true;
-        for (int i = 0; i < (int) toTweet.length(); i+= 140) {
-        	string tweet = toTweet.substr(i, 140);
-        	if (!t.statusUpdate( tweet ))
-        		ok = false;
-        }
-
-        return ok;
+        system(("./main \"" + toTweet + "\"").c_str());
+        return true;
     }
 
     bool TwitterCUD::remove(DiskLoc loc, string ns) {
-    	boost::mutex::scoped_lock lk( _curlLock );
         string toTweet;
 
         toTweet.append(ns);
@@ -179,13 +174,15 @@ namespace mongo {
         toTweet.append(":");
         toTweet.append("Delete");
 
-        return t.statusUpdate( toTweet );
+        system(("./main \"" + toTweet + "\"").c_str());
+        return true;
     }
 
     bool TwitterCUD::custom(string s, string ns) {
-    	boost::mutex::scoped_lock lk( _curlLock );
-    	string tweet = ns + ":" + s;
-    	return t.statusUpdate( tweet );
+    	//boost::mutex::scoped_lock lk( _curlLock );
+    	string toTweet = ns + ":" + s;
+    	system(("./main \"" + toTweet + "\"").c_str());
+    	return true;
     }
 
     string TwitterCUD::_toTweetString(BSONObj obj, DiskLoc loc, string ns) {
